bfd5a6f1de073dc2e6322e454c26f19a6f66f38e
Jonas Silva <jonaslsa@uio.no>
2025-01-17 15:00:05 +0100

    feat: enhance RPC handling with CLI invocation and argument parsing

--- lsp-server/server.ts
+++ lsp-server/server.ts
@@ -26,9 +26,10 @@ import {
 } from 'vscode-languageserver-textdocument';
 
 import { type Definition } from '../rules/queries/schemas/definitions';
-import { executePrologQuery, generatePrologQuery } from '../actions/query';
 import tmp from 'tmp';
 import fs from 'fs';
+import { prepareRPC } from './callRPC';
+import { registeredFunctions } from '../actions/rpc';
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -164,6 +165,8 @@ connection.languages.diagnostics.on(async (params) => {
 
 const documentDefinitions = new Map<string, Definition[]>();
 
+const rpcRunQuery = prepareRPC('query');
+
 async function GetNewDefinitions(document: TextDocument) {
 	const tmpFile = tmp.fileSync();
 
@@ -171,15 +174,13 @@ async function GetNewDefinitions(document: TextDocument) {
 
 	console.log("File written to", tmpFile.name);
 
-	const prolog = await generatePrologQuery(
-		tmpFile.name,
-		'test-dsl/py-like/solution/MyLexer.g4',
-		'test-dsl/py-like/solution/MyParser.g4',
-		'test-dsl/py-like/solution/MyAdapter.pl',
-		'test-dsl/py-like/solution/definitions.pl',
-	);
-
-	return executePrologQuery(prolog.filePath);
+	return await rpcRunQuery({
+		targetPath: tmpFile.name,
+		lexerPath: 'test-dsl/py-like/solution/MyLexer.g4',
+		parserPath: 'test-dsl/py-like/solution/MyParser.g4',
+		adapterPath: 'test-dsl/py-like/solution/MyAdapter.pl',
+		queryPath: 'test-dsl/py-like/solution/definitions.pl',
+	});
 };
 
 // The content of a text document has changed. This event is emitted
