5bd035fcfc13c4aa7e661b4efacac7715c82d93d
Jonas Silva <jonaslsa@uio.no>
2025-01-17 14:20:33 +0100

    feat: add query RPC function and enhance argument handling

--- actions/rpc.ts
+++ actions/rpc.ts
@@ -1,24 +1,31 @@
 import z from 'zod'
 import health from './rpc/health';
+import query from './rpc/query';
 
 export type RPCFunction = {
-    name: string;
-		argumentSchema: z.ZodObject<any>;
-		method: (payload: any) => any;
+	name: string;
+	argumentSchema: z.ZodObject<any> | undefined;
+	method: (payload?: any) => any;
 };
 
-const registeredFunctions: RPCFunction[] = [
-    health,
-];
+export const registeredFunctions = {
+	health,
+	query
+} as const;
+
+export type NamesOfRegisteredFunctions = keyof typeof registeredFunctions;
 
 export function resolveRPC(functionName: string, payload: string) {
-	const registeredFunction = registeredFunctions.find(func => func.name === functionName);
+	const registeredFunction = registeredFunctions[functionName as NamesOfRegisteredFunctions] as RPCFunction;
 	if (!registeredFunction) {
 		console.error(`Function ${functionName} not found`);
 		return;
 	}
 
-	const parsedPayload = registeredFunction.argumentSchema.parse(JSON.parse(payload));
-
-	console.log(registeredFunction.method(parsedPayload));
+	if (!registeredFunction.argumentSchema) {
+		console.log(registeredFunction.method());
+	} else {
+		const parsedPayload = registeredFunction.argumentSchema.parse(JSON.parse(payload));
+		console.log(registeredFunction.method(parsedPayload));
+	}
 }
